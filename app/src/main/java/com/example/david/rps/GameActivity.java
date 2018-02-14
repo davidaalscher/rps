2f06dd812103e3753bea8a882318c83e2f6c5999
askoulwassim <askoulwassim@gmail.com>
2018-02-14 15:58:11 -0600

    determination of player hand choice

--- app/src/main/java/com/example/david/rps/GameActivity.java
+++ app/src/main/java/com/example/david/rps/GameActivity.java
@@ -2,12 +2,78 @@ package com.example.david.rps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
+
+    int rock_value = 0;
+    int paper_value = 0;
+    int scissors_value = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+    public void add_rock() {
+        rock_value = rock_value + 1;
+    }
+
+    public void add_paper() {
+        paper_value = paper_value + 1;
+    }
+
+    public void add_scissors() {
+        scissors_value = scissors_value + 1;
+    }
+
+    int determine_hand(int rock_value, int paper_value, int scissors_value){
+        if (rock_value > paper_value){
+            if (rock_value > scissors_value){
+                return 1;
+            }
+            if (scissors_value > rock_value){
+                return 3;
+            }
+        }
+        if (paper_value > scissors_value){
+            if(paper_value > rock_value){
+                return 2;
+            }
+            if (rock_value > paper_value){
+                return 1;
+            }
+        }
+        if (scissors_value > rock_value){
+            if(scissors_value > paper_value){
+                return 3;
+            }
+            if (paper_value > scissors_value){
+                return 2;
+            }
+        }
+        if(scissors_value == rock_value){
+            if(scissors_value > paper_value){
+                return 1;
+            }
+        }
+        if(scissors_value == paper_value){
+            if(scissors_value > rock_value){
+                return 3;
+            }
+        }
+        if(paper_value == rock_value){
+            if(paper_value > scissors_value){
+                return 2;
+            }
+        }
+
+        Random rand = new Random();
+        int  n = rand.nextInt(3) + 1;
+        return n;
+
+    }
+    
 }
